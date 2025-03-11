6a25437ffc00e04b67941eafe4b5a72133de01cc
Maikol2005 <miguel.garcia@itc-consulting.mx>
2025-03-11 15:08:07 -0600

    add time execution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ from google.cloud import bigquery
 from vertexai.language_models import TextEmbeddingModel
 import numpy as np
 from google.api_core.exceptions import GoogleAPICallError
-import ast
+import time
+
 app = FastAPI()
 
 
@@ -16,6 +17,7 @@ app = FastAPI()
 def procesar_documento(documento: str, topic: str):
     """Extrae el texto del documento, asigna subintenciones y lo almacena en BigQuery"""
     parrafos_con_intenciones = extraer_texto_con_intenciones(documento)
+    promt_determina_intencion = retrieval_get_promt()
     insertar_chunks_en_bigquery(parrafos_con_intenciones, documento, topic)
     print("###### PARRAFOS CON INTENCIONS ####")
     print(parrafos_con_intenciones)
@@ -52,7 +54,9 @@ def buscar(request: SearchRequest):
         # Verificar si el intent es nulo o vacío
         if not request.intent:
             # Convertir la pregunta a embedding
-            question_embedding = embedding_model.get_embeddings([request.question])[0].values
+            question_embedding = embedding_model.get_embeddings([request.question])[
+                0
+            ].values
 
             # Construir la consulta filtrando solo por topic
             query = f"""
@@ -73,15 +77,22 @@ def buscar(request: SearchRequest):
                 norm_b = np.linalg.norm(vec_b)
                 return dot / (norm_a * norm_b) if norm_a and norm_b else 0
 
+            start_time = time.time()  # Inicio de la medición
             # Calcular similitudes para cada fila
             for row in rows:
                 chunk_embedding = row["embedding"]
                 similarity = cosine_similarity(question_embedding, chunk_embedding)
-                similarities.append({
-                    "text": row["text"],  # Solo almacenamos el texto de la respuesta
-                    "similarity": similarity,
-                })
-
+                similarities.append(
+                    {
+                        "text": row[
+                            "text"
+                        ],  # Solo almacenamos el texto de la respuesta
+                        "similarity": similarity,
+                    }
+                )
+            end_time = time.time()  # Fin de la medición
+            time_execution = end_time - start_time
+            print("Tiempo de ejecución:", end_time - start_time, "segundos")
             # Ordenar por similitud (de mayor a menor)
             similarities.sort(key=lambda x: x["similarity"], reverse=True)
 
@@ -91,6 +102,9 @@ def buscar(request: SearchRequest):
             # Retornar el top 3 de respuestas
             return {
                 "response": top_responses,
+                "knowledge_domain": request.topic,
+                "transactional_or_non_transactional": "non_transactional",
+                "time_execution": time_execution,
             }
         else:
             # Construir la consulta filtrando por intent, topic y channel
