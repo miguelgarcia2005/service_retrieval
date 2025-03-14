76096f4c3a6b957aefb55439729347469b18d940
Maikol2005 <miguel.garcia@itc-consulting.mx>
2025-03-13 19:50:46 -0600

    fix new info saldos

--- app/bigquery.py
+++ app/bigquery.py
@@ -18,11 +18,12 @@ def insertar_chunks_en_bigquery(parrafos_con_intenciones, documento,topic):
     """Inserta los chunks extraídos en BigQuery, generando el embedding para cada uno."""
     table_ref = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
     rows = []
+    intents_repeated = ['ModalidadCuarenta','RequisitosModalidadCuarenta','BeneficiosDeLaModalidadCuarenta','PagoModalidadCuarenta','InscripcionModalidadCuarenta','ConsideracioneDeLaModalidadCuarenta']
 
     for i, parrafo in enumerate(parrafos_con_intenciones):
         # Generar el embedding para el párrafo (chunk)
         embedding = embedding_model.get_embeddings([parrafo["texto"]])[0].values
-
+        is_repeat = "S" if parrafo["intencion"] in intents_repeated else "N"
         # Construir el registro con el embedding incluido
         row = {
             "id": f"{topic}_{parrafo['intencion']}_chunk_{i}",
@@ -34,8 +35,9 @@ def insertar_chunks_en_bigquery(parrafos_con_intenciones, documento,topic):
             "intent": parrafo["intencion"],
             "is_transactional": "N",
             "embedding": embedding,
+            "is_repeat" : is_repeat
         }
-        print(f"##### El valor del embedding es: {embedding} ####\n\n")
+        # print(f"##### El valor del embedding es: {embedding} ####\n\n")
         rows.append(row)
 
     errors = bq_client.insert_rows_json(table_ref, rows)
