db6df61e3b34a738d9490b4096a00e4a860e2a41
Maikol2005 <miguel.garcia@itc-consulting.mx>
2025-04-24 15:38:50 -0600

    Update extractor.py

--- app/extractor.py
+++ app/extractor.py
@@ -50,6 +50,11 @@ def extraer_texto_con_intenciones(blob_name):
                 )
 
                 if es_titulo:
+                    if parrafos_con_intenciones and parrafos_con_intenciones[-1]["intencion"] == intencion_actual:
+                        parrafos_con_intenciones.append({
+                                "intencion": intencion_actual,
+                                "texto": line_text
+                            })
                     print(line_text)
                     # Actualizamos la intención con el título detectado
                     intencion_actual = line_text
