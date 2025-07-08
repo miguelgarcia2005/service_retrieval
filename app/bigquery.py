037af3fcceb5cbaa5a55ead77f9a4b04363126d7
Maikol2005 <miguel.garcia@itc-consulting.mx>
2025-07-08 02:22:35 -0600

    Update bigquery.py

--- app/bigquery.py
+++ app/bigquery.py
@@ -2,7 +2,7 @@ from google.cloud import bigquery
 import os
 from dotenv import load_dotenv
 from vertexai.language_models import TextEmbeddingModel
-
+import vertexai
 # Cargar variables de entorno
 load_dotenv()
 PROJECT_ID = os.getenv("PROJECT_ID")
@@ -11,9 +11,14 @@ TABLE_ID = os.getenv("TABLE_ID")
 TABLE_ID_BETA = os.getenv("TABLE_ID_BETA")
 
 # Inicializar el cliente de BigQuery y el modelo de embeddings
-bq_client = bigquery.Client()
-embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko")
+# bq_client = bigquery.Client()
+# embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko")
 
+
+# Inicializar Vertex AI y modelo
+vertexai.init(project=PROJECT_ID, location="us-central1")
+bq_client = bigquery.Client()
+embedding_model = TextEmbeddingModel.from_pretrained("gemini-embedding-001")
 
 def insertar_chunks_en_bigquery(parrafos_con_intenciones, documento, topic, channel):
     """Inserta los chunks extraídos en BigQuery, generando el embedding para cada uno."""
@@ -56,10 +61,9 @@ def insertar_chunks_en_bigquery(parrafos_con_intenciones, documento, topic, chan
         raise Exception(f"Error insertando en BigQuery: {errors}")
 
 
-def insertar_chunks_en_bigquery_beta(
-    parrafos_con_intenciones, documento, topic, channel
-):
-    """Inserta los chunks extraídos en BigQuery, generando el embedding para cada uno."""
+
+def insertar_chunks_en_bigquery_beta(parrafos_con_intenciones, documento, topic, channel):
+    """Inserta los chunks extraídos en BigQuery beta, generando el embedding para cada uno."""
     table_ref = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID_BETA}"
     rows = []
     intents_repeated = [
@@ -75,10 +79,8 @@ def insertar_chunks_en_bigquery_beta(
     ]
 
     for i, parrafo in enumerate(parrafos_con_intenciones):
-        # Generar el embedding para el párrafo (chunk)
         embedding = embedding_model.get_embeddings([parrafo["texto"]])[0].values
         is_repeat = "S" if parrafo["intent"] in intents_repeated else "N"
-        # Construir el registro con el embedding incluido
         row = {
             "id": f"{topic}_{parrafo['intent']}_chunk_{i}",
             "channel": channel.lower().strip(),
@@ -86,14 +88,12 @@ def insertar_chunks_en_bigquery_beta(
             "chunk_id": i,
             "text": parrafo["texto"].strip(),
             "knowledge_domain": topic.lower().strip(),
-            # "knowledge_group": parrafo["subtitle"].strip(),
             "intent": parrafo["intent"].lower().strip(),
             "intent_document": parrafo["intent"].strip(),
             "is_transactional": "N",
             "embedding": embedding,
             "is_repeat": is_repeat,
         }
-        # print(f"##### El valor del embedding es: {embedding} ####\n\n")
         rows.append(row)
 
     errors = bq_client.insert_rows_json(table_ref, rows)
